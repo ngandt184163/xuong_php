4301f512e376554ce3bae6eef2d3bcf9e385b341
ngandt184163 <ngan.dt184163@sis.hust.edu.vn>
2024-03-16 13:47:30 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,155 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Trang chu</title>
+    <link rel="stylesheet" href="./css/header.css">
+    <link rel="stylesheet" href="./css/footer.css">
+    <link rel="stylesheet" href="./css/container.css">
+
+</head>
+<body>
+
+    <div class="header">
+        <div class="menu-1">
+            <ul>
+                <li><a href="#">Kenh nguoi ban</a></li>
+                <li><a href="#">Tai ung dunh</a></li>
+                <li><a href="#">ket noi</a></li>
+                <li><a href="#"><i class="fa-brands fa-facebook"></i></a></li>
+            </ul>
+
+            <ul>
+                <li><a href="#">thong bao</a></li>
+                <li><a href="#">ho tro</a></li>
+                <li><a href="#">tieng viet</a></li>
+                <li><a href="#">user</a></li>
+            </ul>
+        </div>
+
+        <div class="menu-2">
+            <div class="logo">
+                <img with='100px' height="50px" src="./img/logo.png" alt="logo shopee">
+            </div>
+
+            <div class="search">
+                <input type="text" placeholder="Search for products...">
+                <i class="fa-solid fa-magnifying-glass"></i>
+            </div>
+
+            <div class="cart">
+                <i class="fa-solid fa-cart-shopping"></i>
+            </div>
+        </div>
+
+        <div class="menu-3">
+            <ul>
+                <li><a href="#">giay vs</a></li>
+                <li><a href="#">ao mua</a></li>
+                <li><a href="#">moc quan ao</a></li>
+                <li><a href="#">giay vs cao cap</a></li>
+                <li><a href="#">ruot but bi</a></li>
+                <li><a href="#">bitis hunter</a></li>
+                <li><a href="#">giay nam</a></li>
+                <li><a href="#">ao mua</a></li>
+                <li><a href="#">mu bao hiem</a></li>
+            </ul>
+        </div>
+
+    </div>
+
+    <div class="container">
+        <div class="product">
+            <div class="img">
+                <img width="100px" height="50px" src="https://image.anhducdigital.vn/apple/iphone/iphone-15-pro-15-pro-max/iphone-15-pro-max/iphone-15-pro-max-28.jpg" alt="">
+            </div>
+
+            <div class="title">
+                <h2>IP 15 promax</h2>
+            </div>
+
+            <div class="info">
+                <b>20 000 000</b>
+                <i>Da ban 20</i>
+            </div>
+        </div>
+        <div class="product">
+            <div class="img">
+                <img width="100px" height="50px" src="https://image.anhducdigital.vn/apple/iphone/iphone-15-pro-15-pro-max/iphone-15-pro-max/iphone-15-pro-max-28.jpg" alt="">
+            </div>
+
+            <div class="title">
+                <h2>IP 15 promax</h2>
+            </div>
+
+            <div class="info">
+                <b>20 000 000</b>
+                <i>Da ban 20</i>
+            </div>
+        </div>
+        <div class="product">
+            <div class="img">
+                <img width="100px" height="50px" src="https://image.anhducdigital.vn/apple/iphone/iphone-15-pro-15-pro-max/iphone-15-pro-max/iphone-15-pro-max-28.jpg" alt="">
+            </div>
+
+            <div class="title">
+                <h2>IP 15 promax</h2>
+            </div>
+
+            <div class="info">
+                <b>20 000 000</b>
+                <i>Da ban 20</i>
+            </div>
+        </div>
+        <div class="product">
+            <div class="img">
+                <img width="100px" height="50px" src="https://image.anhducdigital.vn/apple/iphone/iphone-15-pro-15-pro-max/iphone-15-pro-max/iphone-15-pro-max-28.jpg" alt="">
+            </div>
+
+            <div class="title">
+                <h2>IP 15 promax</h2>
+            </div>
+
+            <div class="info">
+                <b>20 000 000</b>
+                <i>Da ban 20</i>
+            </div>
+        </div>
+        <div class="product">
+            <div class="img">
+                <img width="100px" height="50px" src="https://image.anhducdigital.vn/apple/iphone/iphone-15-pro-15-pro-max/iphone-15-pro-max/iphone-15-pro-max-28.jpg" alt="">
+            </div>
+
+            <div class="title">
+                <h2>IP 15 promax</h2>
+            </div>
+
+            <div class="info">
+                <b>20 000 000</b>
+                <i>Da ban 20</i>
+            </div>
+        </div>
+        <div class="product">
+            <div class="img">
+                <img width="100px" height="50px" src="https://image.anhducdigital.vn/apple/iphone/iphone-15-pro-15-pro-max/iphone-15-pro-max/iphone-15-pro-max-28.jpg" alt="">
+            </div>
+
+            <div class="title">
+                <h2>IP 15 promax</h2>
+            </div>
+
+            <div class="info">
+                <b>20 000 000</b>
+                <i>Da ban 20</i>
+            </div>
+        </div>
+    </div>
+
+    <div class="footer">
+        <b>Xuong thuc hanh php</b>
+    </div>
+    
+</body>
+<script src="https://kit.fontawesome.com/791d1d50ef.js" crossorigin="anonymous"></script>
+</html>
